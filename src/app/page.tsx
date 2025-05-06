0a07e33b0c4c50230df5ea7849453670559c3566
Jaqueline Batista Vieira✭・ <jackebatista07@hotmail.com>
2025-05-06 15:51:01 -0300

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-[black] h-[100vh] flex items-center justify-center">
+      <h1 className="text-[white] text-5x1">estudos e ódios</h1>
+    </div>
+  );
+}
